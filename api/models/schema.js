8f276303de0c296d35e722f6d9a7ed78d3947da9
Harsh Tomar <harshtomar6@gmail.com>
2018-03-31 23:38:39 +0530

    Added Visitors

--- api/models/schema.js
+++ api/models/schema.js
@@ -4,7 +4,7 @@ let mongoose = require('mongoose');
 // Visitor Schema
 let visitorSchema = mongoose.Schema({
   ip: {type: String, required: true},
-  timestamp: {type: Date, default: Date.now()},
+  timestamp: {type: Date, default: Date.now},
   useragent: {type: String, required: true},
   page: {type: String, required: true}
 });
@@ -12,7 +12,7 @@ let visitorSchema = mongoose.Schema({
 // Anonymous Schema
 let anonymousSchema = mongoose.Schema({
   ip: {type: String, required: true},
-  timestamp: {type: Date, default: Date.now()},
+  timestamp: {type: Date, default: Date.now},
   os: {type: String, required: true},
   client: {type: String, required: true},
   serviceProvider: {type: String, required: true},
